8730c48a95fd01e052602aa51187bfde1a1a474b
RebekahEast <rebekah.jean.east@gmail.com>
2023-02-28 18:37:08 +0000

    using the up/down key

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
+//instruction popup
 function instruction() {
   var ShowInstructions = document.getElementById("PopUpInstructions");
   ShowInstructions.classList.toggle("show");
 }
 
-window.onload = function () {
-  GameSetUp ();
+//when pressing a key on the keyboard, an alert pops up
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    console.log("ehy");
+  } else if (event.key === "ArrowDown") {
+    console.log("yhe");
+  } else if (event.key === "ArrowRight") {
+    handleArrowKeyPress();
+  } else {
+    console.log("yeh");
   }
-
-  function GameSetUp () {
-    
-  }
-
-
+  console.log("al done!");
+});
 
   
