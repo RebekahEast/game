2a63272594175bc34b83458052fd17b1262d575d
RebekahEast <rebekah.jean.east@gmail.com>
2023-03-08 15:22:20 +0000

    delayed left and right work

--- tests/test2.js
+++ tests/test2.js
@@ -6,7 +6,7 @@ document.addEventListener("keydown", (event) => {
       handleArrowDownPress();
 
     } else if (event.key === "ArrowRight") { 
-      handleArrowRightPress();
+        delayedHandleArrowRightPress();
 
     } else if (event.key === "ArrowLeft") { 
         delayedHandleArrowLeftPress();
@@ -15,16 +15,6 @@ document.addEventListener("keydown", (event) => {
     }
     console.log("a key was pressed!");
   });
-
-function handleArrowLeftPress() {
-    var rows = document.getElementsByClassName("row");
-    rows[0].children[3].classList.remove("number-2");
-    rows[0].children[2].classList.add("number-2");
-    rows[0].children[2].classList.remove("number-2");
-    rows[0].children[1].classList.add("number-2");
-    rows[0].children[1].classList.remove("number-2");
-    rows[0].children[0].classList.add("number-2");
-}
   
 function handleArrowRightPress() {
     var rows = document.getElementsByClassName("row");
@@ -36,28 +26,45 @@ function handleArrowRightPress() {
     rows[0].children[3].classList.add("number-2");
 }
 
+function delayedHandleArrowRightPress() {
+    var rows = document.getElementsByClassName("row");
+    setTimeout(function() {
+        rows[0].children[0].classList.remove("number-2");
+    }, 25); 
+    setTimeout(function() {
+        rows[0].children[1].classList.add("number-2");
+    }, 50); 
+    setTimeout(function() {
+        rows[0].children[1].classList.remove("number-2");
+    }, 75); 
+    setTimeout(function() {
+        rows[0].children[2].classList.add("number-2");
+    }, 100); 
+    setTimeout(function() {
+        rows[0].children[2].classList.remove("number-2");
+    }, 125); 
+    setTimeout(function() {
+        rows[0].children[3].classList.add("number-2");
+    }, 150); 
+  }
+
 function delayedHandleArrowLeftPress() {
     var rows = document.getElementsByClassName("row");
     setTimeout(function() {
         rows[0].children[3].classList.remove("number-2");
     }, 25); 
-  
     setTimeout(function() {
         rows[0].children[2].classList.add("number-2");
     }, 50); 
-  
     setTimeout(function() {
         rows[0].children[2].classList.remove("number-2");
     }, 75); 
-
     setTimeout(function() {
         rows[0].children[1].classList.add("number-2");
     }, 100); 
-  
     setTimeout(function() {
         rows[0].children[1].classList.remove("number-2");
     }, 125); 
-  
     setTimeout(function() {
         rows[0].children[0].classList.add("number-2");
     }, 150); 
