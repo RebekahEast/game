d9cc94c069dfe1cb2aa90d64f96273d98629bc8a
RebekahEast <rebekah.jean.east@gmail.com>
2023-03-08 10:48:11 +0000

    set up an up to add class number-2

---
+++ tests/test1.js
@@ -0,0 +1,43 @@
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp") { //when the arrow up is pressed I want a 2 to appear
+      handleArrowUpPress()
+    } else if (event.key === "ArrowDown") { //when down is pressed I want a 2 to disappear
+      handleArrowDownPress();
+    } else if (event.key === "ArrowRight") { //when right is pressed I want to move all 2s to the right
+      handleArrowRightPress();
+    } else if (event.key === "ArrowLeft") { //when left is pressed I want to move all 2s to the left
+      handleArrowLeftPress();
+    } else {
+      console.log("yeh");
+    }
+    console.log("a key was pressed!");
+  });
+  
+  
+  function handleArrowRightPress() {
+  }
+  
+  function handleArrowDownPress() {
+    var Number2 = document.getElementsByClassName("number-2");
+    Number2[0].classList.toggle("number-2");
+  }
+  
+  // adds the class 'number-2' to a random cell on the 1st column
+  //BUT it doesn't take into account full classes already when randomising
+  //how could you add children to randomise too?
+  function handleArrowUpPress() {
+    var rows = document.getElementsByClassName("row");
+    const random = Math.floor(Math.random() * rows.length); //creates random for the rows
+    rows[random].children[1].classList.add("number-2");
+  }
+
+  function handleArrowLeftPress() {
+    var rows = document.getElementsByClassName("row");
+    rows[0].children[3].classList.remove("number-2");
+    rows[0].children[2].classList.add("number-2");
+    rows[0].children[2].classList.remove("number-2");
+    rows[0].children[1].classList.add("number-2");
+    rows[0].children[1].classList.remove("number-2");
+    rows[0].children[0].classList.add("number-2");
+  }
+  
