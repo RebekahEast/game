79da3572cf09d3cc9efac2ec2b036f83e84fecad
RebekahEast <rebekah.jean.east@gmail.com>
2023-03-08 14:06:04 +0000

    right arrow and left arrow moving the 2 from left and right

--- tests/test1.js
+++ tests/test1.js
@@ -13,25 +13,24 @@ document.addEventListener("keydown", (event) => {
     console.log("a key was pressed!");
   });
   
-  
-  function handleArrowRightPress() {
-  }
-  
-  function handleArrowDownPress() {
+
+function handleArrowDownPress() {
     var Number2 = document.getElementsByClassName("number-2");
     Number2[0].classList.toggle("number-2");
-  }
+}
   
   // adds the class 'number-2' to a random cell on the 1st column
   //BUT it doesn't take into account full classes already when randomising
   //how could you add children to randomise too?
-  function handleArrowUpPress() {
+function handleArrowUpPress() {
     var rows = document.getElementsByClassName("row");
     const random = Math.floor(Math.random() * rows.length); //creates random for the rows
-    rows[random].children[1].classList.add("number-2");
-  }
+    rows[random].children[3].classList.add("number-2");
+}
 
-  function handleArrowLeftPress() {
+
+// this moves only the top row. how could I say all?... think
+function handleArrowLeftPress() {
     var rows = document.getElementsByClassName("row");
     rows[0].children[3].classList.remove("number-2");
     rows[0].children[2].classList.add("number-2");
@@ -39,5 +38,14 @@ document.addEventListener("keydown", (event) => {
     rows[0].children[1].classList.add("number-2");
     rows[0].children[1].classList.remove("number-2");
     rows[0].children[0].classList.add("number-2");
-  }
+}
   
+function handleArrowRightPress() {
+    var rows = document.getElementsByClassName("row");
+    rows[0].children[0].classList.remove("number-2");
+    rows[0].children[1].classList.add("number-2");
+    rows[0].children[1].classList.remove("number-2");
+    rows[0].children[2].classList.add("number-2");
+    rows[0].children[2].classList.remove("number-2");
+    rows[0].children[3].classList.add("number-2");
+}
